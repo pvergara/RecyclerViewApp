084d00f8e3607b0a8318c33b54254111506cfdaf
pvergara <ecos.logic.org@gmail.com>
2023-12-18 19:20:03 +0100

    Fighting with locales

--- app/src/main/java/org/ecos/logic/recyclerviewapp/MainActivity.java
+++ app/src/main/java/org/ecos/logic/recyclerviewapp/MainActivity.java
@@ -3,12 +3,20 @@ package org.ecos.logic.recyclerviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.action = this.findViewById(R.id.button);
+
+        this.action.setText(R.string.button1);
     }
 }
